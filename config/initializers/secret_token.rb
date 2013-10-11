8a495dfa509f324d3b035de58e1947c0c921a463
Tim McEwan <tim@mcewan.it>
2013-10-11 21:57:39 +1100

    pristine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyFoulTest::Application.config.secret_token = '32395fd763438edccb87b5cd477f411989e38bd025e195a82863bae4a0fd900e6779927222b09eaeefcaf4cc1d370153ac1178c521b1ed5e1349e2bf581ef382'
